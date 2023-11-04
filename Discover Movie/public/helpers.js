1a81f34c114a336bee1632a035c4f2b126b4398f
Mihai20000 <117591224+Mihai20000@users.noreply.github.com>
2023-11-04 19:25:03 +0200

    Added all files of the project

---
+++ Discover Movie/public/helpers.js
@@ -0,0 +1,129 @@
+// Populate dropdown menu with all the available genres
+const populateGenreDropdown = (genres) => {
+    const select = document.getElementById('genres')
+
+    for (const genre of genres) {
+        let option = document.createElement("option");
+        option.value = genre.id;
+        option.text = genre.name;
+        select.appendChild(option);
+    }
+};
+
+// Returns the current genre selection from the dropdown menu
+const getSelectedGenre = () => {
+    const selectedGenre = document.getElementById('genres').value;
+    return selectedGenre;
+};
+
+// Displays the like and dislike buttons on the page
+const showBtns = () => {
+    const btnDiv = document.getElementById('likeOrDislikeBtns');
+    btnDiv.removeAttribute('hidden');
+};
+
+// Clear the current movie from the screen
+const clearCurrentMovie = () => {
+    const moviePosterDiv = document.getElementById('moviePoster');
+    const movieTextDiv = document.getElementById('movieText');
+    const movieRatingDiv = document.getElementById('movieRating');
+    const movieViewsDiv = document.getElementById('movieViews');
+    moviePosterDiv.innerHTML = '';
+    movieTextDiv.innerHTML = '';
+    movieRatingDiv.innerHTML = '';
+    movieViewsDiv.innerHTML = ''; 
+}
+
+// After liking a movie, clears the current movie from the screen and gets another random movie
+const likeMovie = () => {
+    clearCurrentMovie();
+    showRandomMovie();
+};
+
+// After disliking a movie, clears the current movie from the screen and gets another random movie
+const dislikeMovie = () => {
+    clearCurrentMovie();
+    showRandomMovie();
+};
+
+// Create HTML for movie poster
+const createMoviePoster = (posterPath) => {
+    const moviePosterUrl = `https://image.tmdb.org/t/p/original/${posterPath}`;
+
+    const posterImg = document.createElement('img');
+    posterImg.setAttribute('src', moviePosterUrl);
+    posterImg.setAttribute('id', 'moviePoster');
+  
+    return posterImg;
+};
+
+// Create HTML for movie title
+const createMovieTitle = (title) => {
+    const titleHeader = document.createElement('h1');
+    titleHeader.setAttribute('id', 'movieTitle');
+    titleHeader.innerHTML = title;
+  
+    return titleHeader;
+};
+
+// Create HTML for movie rating
+const createMovieRating = (rating) => {
+    const ratingText = document.createElement('h3');
+    ratingText.setAttribute('id', 'movieRating');
+    ratingText.innerHTML = rating + ' average rating';
+
+    return ratingText;
+}
+
+// Create HTML for movie rating
+const createMovieViews = (views) => {
+    const viewsText = document.createElement('p');
+    viewsText.setAttribute('id', 'movieViews');
+    viewsText.innerHTML = views + ' views';
+
+    return viewsText;
+}
+
+// Create HTML for movie overview
+const createMovieOverview = (overview) => {
+    const overviewParagraph = document.createElement('p');
+    overviewParagraph.setAttribute('id', 'movieOverview');
+    overviewParagraph.innerHTML = overview;
+  
+    return overviewParagraph;
+};
+
+// Returns a random movie from the first page of movies
+const getRandomMovie = (movies) => {
+    const randomIndex = Math.floor(Math.random() * movies.length);
+    const randomMovie = movies[randomIndex];
+    return randomMovie;
+};
+
+// Uses the DOM to create HTML to display the movie
+const displayMovie = (movieInfo) => {
+    const moviePosterDiv = document.getElementById('moviePoster');
+    const movieTextDiv = document.getElementById('movieText');
+    const likeBtn = document.getElementById('likeBtn');
+    const dislikeBtn = document.getElementById('dislikeBtn');
+    const movieRatingDiv = document.getElementById('movieRating');
+    const movieViewsDiv = document.getElementById('movieViews');
+  
+    // Create HTML content containing movie info
+    const moviePoster = createMoviePoster(movieInfo.poster_path);
+    const titleHeader = createMovieTitle(movieInfo.title);
+    const overviewText = createMovieOverview(movieInfo.overview);
+    const ratingText = createMovieRating(movieInfo.vote_average);
+    const viewsText = createMovieViews(movieInfo.popularity);
+  
+    // Append title, poster, and overview to page
+    moviePosterDiv.appendChild(moviePoster);
+    movieTextDiv.appendChild(titleHeader);
+    movieTextDiv.appendChild(overviewText);
+    movieRatingDiv.appendChild(ratingText);
+    movieViewsDiv.appendChild(viewsText);
+  
+    showBtns();
+    likeBtn.onclick = likeMovie;
+    dislikeBtn.onclick = dislikeMovie;
+};
